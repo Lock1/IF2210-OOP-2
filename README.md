d34832a435635d7303175d4e38d1b8a270d4e5d0
16519111 <gregdimasbaskara@gmail.com>
2021-04-16 18:52:38 +0700

    initial push gdx

--- README.md
+++
@@ -1,9 +0,0 @@
-# OOP-2-IF2220
-Tugas Besar - IF2220 - Pemrograman Berbasis Objek \
-
-**TBA**
-
-Tools         | Version
------         | --------
-JDK           | 15.0.2
-Intellij IDEA | 2020.2.2
--- src/Supports/ScreenSettings.java
+++
@@ -1,9 +0,0 @@
-package Supports;
-
-import javafx.stage.Stage;
-import java.awt.*;
-
-public class ScreenSettings {
-    public static Stage pStage;
-    public static Dimension mediaSize = Toolkit.getDefaultToolkit().getScreenSize();
-}
