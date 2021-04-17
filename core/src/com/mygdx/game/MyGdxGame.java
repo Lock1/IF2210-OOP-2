1e844b2b07247722f057cb9acb6ee03a7aa8071f
16519111 <gregdimasbaskara@gmail.com>
2021-04-17 17:12:32 +0700

    Add MainMenuScreen and PauseScreen

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,104 +1,19 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.NinePatch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
-import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.Game;
+import com.mygdx.game.screens.MainMenuScreen;
 
-public class MyGdxGame extends ApplicationAdapter {
-	Stage stage;
-	SpriteBatch batch;
-	Texture img;
-	Label titleLabel;
-	Table table;
-	
-	@Override
+public class MyGdxGame extends Game {
+
 	public void create () {
-		int row_height = Gdx.graphics.getWidth() / 12;
-		stage = new Stage(new ScreenViewport());
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-
-		// Title Label
-		Label.LabelStyle titleLabelStyle = new Label.LabelStyle();
-		titleLabelStyle.font = new BitmapFont();
-		titleLabelStyle.fontColor = Color.BLACK;
-		titleLabel = new Label("Engimon Factory", titleLabelStyle);
-		titleLabel.setSize(Gdx.graphics.getWidth(),row_height);
-		titleLabel.setPosition(0,Gdx.graphics.getHeight()-row_height*2);
-		titleLabel.setAlignment(Align.center);
-		stage.addActor(titleLabel);
-
-		Label newLabel = new Label("New\nGame", titleLabelStyle);
-		newLabel.setAlignment(Align.center);
-		Label loadLabel = new Label("Load\nGame", titleLabelStyle);
-		loadLabel.setAlignment(Align.center);
-		Label helpLabel = new Label("Help", titleLabelStyle);
-		helpLabel.setAlignment(Align.center);
-		Label exitLabel = new Label("Exit", titleLabelStyle);
-		exitLabel.setAlignment(Align.center);
-
-		NinePatch patch = new NinePatch(new Texture(Gdx.files.internal("background-tall.png")),
-				1, 1, 3, 3);
-		NinePatchDrawable background = new NinePatchDrawable(patch);
-
-		NinePatch patch2 = new NinePatch(new Texture(Gdx.files.internal("background.png")),
-				1, 1, 1, 1);
-		NinePatchDrawable background2 = new NinePatchDrawable(patch);
-
-		table = new Table();
-
-		Table tableNewGame = new Table();
-		tableNewGame.add(newLabel);
-		tableNewGame.setBackground(background);
-		table.add(tableNewGame).width(100).height(200);
-
-		Table tableLoadGame = new Table();
-		tableLoadGame.add(loadLabel);
-		tableLoadGame.setBackground(background);
-		table.add(tableLoadGame).width(100).height(200);
-
-		Table tableHelp = new Table();
-		tableHelp.add(helpLabel);
-		tableHelp.setBackground(background2);
-
-		Table tableExit = new Table();
-		tableExit.add(exitLabel);
-		tableExit.setBackground(background2);
-
-		Table table2 = new Table();
-		table2.add(tableHelp).height(100).width(100);
-		table2.row();
-		table2.add(tableExit).height(100).width(100);
-		table.add(table2);
-		table.setFillParent(true);
-
-		stage.addActor(table);
+		this.setScreen(new MainMenuScreen(this));
 	}
 
-	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		stage.act();
-		stage.draw();
-		batch.end();
+		super.render();
 	}
-	
-	@Override
+
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
---
+++ core/src/com/mygdx/game/screens/MainMenuScreen.java
@@ -0,0 +1,157 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class MainMenuScreen implements Screen {
+    private Stage stage;
+    private Game game;
+    private SpriteBatch batch;
+    private Texture img;
+    private Label titleLabel;
+    private Table table;
+
+    public MainMenuScreen(Game aGame) {
+        // Setup Stage
+        game = aGame;
+        stage = new Stage(new ScreenViewport());
+
+        int row_height = Gdx.graphics.getWidth() / 12;
+        stage = new Stage(new ScreenViewport());
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+
+        // Style untuk Label
+        Label.LabelStyle titleLabelStyle = new Label.LabelStyle();
+        titleLabelStyle.font = new BitmapFont();
+        titleLabelStyle.fontColor = Color.BLACK;
+
+        // Title Label
+        titleLabel = new Label("Engimon Factory", titleLabelStyle);
+        titleLabel.setSize(Gdx.graphics.getWidth(),row_height);
+        titleLabel.setPosition(0,Gdx.graphics.getHeight()-row_height*2);
+        titleLabel.setAlignment(Align.center);
+        stage.addActor(titleLabel);
+
+        // Style untuk TextButtons
+        TextButton.TextButtonStyle menuButtonStyle = new TextButton.TextButtonStyle();
+        menuButtonStyle.font = new BitmapFont();
+        menuButtonStyle.fontColor = Color.BLACK;
+
+        // Definisi dan Implementasi TextButtons
+        TextButton newButton = new TextButton("New\nGame", menuButtonStyle);
+        newButton.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new PauseScreen(game));
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+        TextButton loadButton = new TextButton("Load\nGame", menuButtonStyle);
+
+        TextButton helpButton = new TextButton("Help", menuButtonStyle);
+
+        TextButton exitButton = new TextButton("Exit", menuButtonStyle);
+
+        // NinePatch untuk border Buttons
+        NinePatch patch = new NinePatch(new Texture(Gdx.files.internal("background-tall.png")),
+                1, 1, 3, 3);
+        NinePatchDrawable background = new NinePatchDrawable(patch);
+
+        NinePatch patch2 = new NinePatch(new Texture(Gdx.files.internal("background.png")),
+                1, 1, 1, 1);
+        NinePatchDrawable background2 = new NinePatchDrawable(patch);
+
+        // Tables untuk menyusun TextButtons
+        table = new Table();
+
+        Table tableNewGame = new Table();
+        tableNewGame.add(newButton);
+        tableNewGame.setBackground(background);
+        table.add(tableNewGame).width(100).height(200);
+
+        Table tableLoadGame = new Table();
+        tableLoadGame.add(loadButton);
+        tableLoadGame.setBackground(background);
+        table.add(tableLoadGame).width(100).height(200);
+
+        Table tableHelp = new Table();
+        tableHelp.add(helpButton);
+        tableHelp.setBackground(background2);
+
+        Table tableExit = new Table();
+        tableExit.add(exitButton);
+        tableExit.setBackground(background2);
+
+        Table table2 = new Table();
+        table2.add(tableHelp).height(100).width(100);
+        table2.row();
+        table2.add(tableExit).height(100).width(100);
+        table.add(table2);
+        table.setFillParent(true);
+
+        // Menambahkan table ke dalam stage
+        stage.addActor(table);
+    }
+
+    @Override
+    public void show() {
+        Gdx.app.log("MainScreen","show");
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        // Merender Batches dan Stages
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        stage.act();
+        stage.draw();
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
